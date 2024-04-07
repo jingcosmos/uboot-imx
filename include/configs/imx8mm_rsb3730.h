c9861bb00261ddde13e76f4076bf48afbb0fb708
kuihong.xin <kuihong.xin@advantech.com.cn>
2024-04-07 17:18:14 +0800

    [3730] fixed .h build 4G warining

--- include/configs/imx8mm_rsb3730.h
+++ include/configs/imx8mm_rsb3730.h
@@ -174,7 +174,6 @@
 
 #define CFG_SYS_SDRAM_BASE           0x40000000
 #define PHYS_SDRAM                      0x40000000
-#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
 
 #if defined(CONFIG_TARGET_IMX8MM_RSB3730A2_2G)
 #define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
